a0cd5d771d8d37259b993c55f7eca1ad64979fe1
Mona <mona.liu@mail.mcgill.ca>
2025-07-22 23:58:51 -0400

    make 404 error page

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
   eleventyConfig.addPassthroughCopy({ 'src/styles': 'styles' });
   eleventyConfig.addPassthroughCopy({ 'src/uploads': 'uploads' });
+  eleventyConfig.addPassthroughCopy({ 'src/.htaccess': '.htaccess' });
 
 
   // change input and output folders
